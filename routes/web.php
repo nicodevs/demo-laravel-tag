7888df9e89e68e20bdf83fae8c0c02d778580c8d
Nico Devs <nico1485@gmail.com>
2026-08-14 14:12:22 -0300

    Exclude webhook from ValidateCsrfToken

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\SlackEventsController;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/slack/events', SlackEventsController::class);
+Route::post('/slack/events', SlackEventsController::class)
+    ->withoutMiddleware(ValidateCsrfToken::class);
